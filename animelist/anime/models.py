728d08bce1189977df3eeed85ccfc76bd7a52c99
Jacob Dela Roca <156392871+jacobdelaroca@users.noreply.github.com>
2024-02-18 13:14:32 +0800

    Added the create updata and delete endpoints Added detail view of anime in users list and anime in the top 50 Modified the Anime model to utilize a foriegn key to the Animes model

--- animelist/anime/models.py
+++ animelist/anime/models.py
@@ -2,25 +2,44 @@ from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
 
+
+STATUS_CHOICES = [
+    ('Watching', 'Watching'),
+    ('Finished', 'Finished'),
+    ('Plan to Watch', 'Plan to Watch'),
+]
+
 # Create your models here.
-class Anime(models.Model):
-    name = models.CharField(max_length=150)
-    genre = models.CharField(max_length=200)
-    rating = models.IntegerField()
-    favorite = models.BooleanField()
-    comment = models.CharField(max_length=1000)
-    current_episode = models.IntegerField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.CharField(max_length=50)
-
-    def __str__(self) -> str:
-        return self.name
-
 class Animes(models.Model):
     name = models.CharField(max_length=200)
     genres = models.CharField(max_length=200)
     num_of_eps = models.IntegerField()
     img = models.CharField(max_length=200)
 
+    def __str__(self) -> str:
+        return self.name
+
+
+class Anime(models.Model):
+    anime = models.ForeignKey(Animes, on_delete=models.CASCADE, null=True)
+    rating = models.IntegerField(default=0)
+    favorite = models.BooleanField(default=False)
+    comment = models.CharField(max_length=1000, default='')
+    current_episode = models.IntegerField(default=1)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Watching')
+
+    def __str__(self) -> str:
+        return self.anime.name
+    
+
+class PublicAnimelist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+
 admin.site.register(Anime)
 admin.site.register(Animes)
+admin.site.register(PublicAnimelist)
--- animelist/anime/serializers.py
+++ animelist/anime/serializers.py
@@ -1,11 +1,24 @@
 from .models import Anime, Animes
+from .models import *
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+class AnimesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Animes
+        fields = ['id', 'name', 'genres', 'num_of_eps', 'img']
+
+
 class AnimeSerializer(serializers.ModelSerializer):
+    # anime = AnimesSerializer()
+    name = serializers.CharField(source='anime.name')
+    genres = serializers.CharField(source='anime.genres')
+    num_of_eps = serializers.CharField(source='anime.num_of_eps')
+    img = serializers.CharField(source='anime.img')
+    owner = serializers.CharField(source='owner.username')
     class Meta:
         model = Anime
-        fields = ['name', 'genre', 'rating', 'favorite', 'comment', 'current_episode', 'owner', 'status']
+        fields = ['id', 'name', 'genres', 'num_of_eps', 'img', 'rating', 'favorite', 'comment', 'current_episode', 'owner', 'status']
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -17,4 +30,12 @@ class UserSerializer(serializers.ModelSerializer):
 class AnimeListSetializer(serializers.ModelSerializer):
     class Meta(object):
         model = Animes
-        fields = ['name', 'genres', 'num_of_eps', 'img']
+        fields = ['id', 'name', 'genres', 'num_of_eps', 'img']
+
+
+class PublicListSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username')
+    id = serializers.IntegerField(source='user.id')
+    class Meta:
+        model = PublicAnimelist
+        fields = ['user', 'id']
